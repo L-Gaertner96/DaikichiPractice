a90fe2b0290a0b5080314e1abca279213b337986
L-Gaertner96 <120612393+L-Gaertner96@users.noreply.github.com>
2023-06-19 19:26:49 -0400

    Add files via upload

---
+++ DaikichiPractice/src/main/java/com/logan/daikichiPractice/DaikichiPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.logan.daikichiPractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaikichiPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaikichiPracticeApplication.class, args);
+	}
+
+}
---
+++ DaikichiPractice/src/test/java/com/logan/daikichiPractice/DaikichiPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logan.daikichiPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaikichiPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
